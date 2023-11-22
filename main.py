0f013e7c046aed63d2ce99db76b787aae2ea8dff
progoz <01169229@pw.edu.pl>
2023-11-22 17:17:01 +0100

    Upload New File

---
+++ main.py
@@ -0,0 +1,18 @@
+from jsonOperations import getObjectsFromJson
+from languageChecker import checkLanguage
+
+artists = getObjectsFromJson()
+
+def filterGenres(artists, func=checkLanguage):
+    for artist in artists:
+        filteredLanguages = []
+        for genre in artist['genres']:
+            language = checkLanguage(genre)
+            if language == 'EN':
+                filteredLanguages.append(genre)
+            else:
+                pass
+        artist['genres'] = filteredLanguages
+    return artists
+
+print(filterGenres(artists))
