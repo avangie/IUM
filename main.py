7ed11a335473a2e3ea7843c775046db02deca3d4
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2023-12-27 20:03:38 +0100

    Changed how it works

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ artists = getObjectsFromJson('artists.jsonl')
 new_artists = getObjectsFromJson('artists_modified.jsonl')
 
 number = 15
-words = ['rock', 'k-pop', 'pop', 'hip hop', 'folk', 'jazz', 'country', 'reggae', 'rap', 'soul', 'punk', 'house', 'trap', 'metal', 'techno', 'indie']
+words = ['rock', 'k-pop', 'pop', 'hip hop', 'folk', 'jazz', 'country', 'reggae', 'rap', 'soul', 'punk', 'house', 'trap', 'metal', 'techno', 'indie', 'opm']
 dict = artists.copy()
 
 for word in words:
     list = findGroups(dict, word)
     dict = replaceToGenres(dict, list, word)
+
+popularity = findPopularity(dict)
+dict = chooseOnlyOneGenre(dict, popularity)
 low_occurs_genders = getEveryGenreBelowOccursNumber(dict, number)
 dict = removeFromGenres(dict, low_occurs_genders)
 dict = cleanGenres(dict)
-popularity = findPopularity(dict)
-dict = chooseOnlyOneGenre(dict, popularity)
 dict = cleanArtists(dict)
 writeToNewJsonFile2(dict)
 
-# print(sorted(findPopularity(dict)))
-
 
 print(len(genresToDict(new_artists)))
-# print(len(getEveryGenreBelowOccursNumber(artists, 15)))
+print(genresToDict(new_artists))
